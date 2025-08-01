34b5e786259a4bd5462e31653ed24896fd10adf0
ad-altun <adaltun@web.de>
2025-08-01 14:12:28 +0200

    typescript-exercise: add takeExam dunction

--- typescript-exercise/src/students.ts
+++ typescript-exercise/src/students.ts
@@ -13,31 +13,64 @@ type Student = {
   courses: string[]; // List of courses the student is enrolled in
   isEnrolled: boolean; // Enrollment status of the student
   grades: (number | undefined)[]; // Array of grades the student has received
+  takeExam: (grade: number) => boolean;
 };
 
-const student1: Student = {
-  name: "Anton Meier",
-  age: 16,
-  courses: ["Math", "Science", "History"],
-  isEnrolled: true,
-  grades: [Grade.Sufficient, undefined, Grade.Satisfactory, Grade.Satisfactory],
+const createStudent = (
+  name: string, // Name of the student
+  age: number,
+  courses: string[],
+  isEnrolled: boolean,
+  grades: (number | undefined)[]
+): Student => {
+  return {
+    name,
+    age,
+    courses,
+    isEnrolled,
+    grades,
+    takeExam(grade: number) {
+      if (typeof grade === "number" && Object.values(Grade).includes(grade)) {
+        this.grades.push(grade);
+        return true;
+      }
+      return false;
+    },
+  };
 };
 
-const student2: Student = {
-  name: "Berta Müller",
-  age: 15,
-  courses: ["Math", "Music", "History"],
-  isEnrolled: true,
-  grades: [Grade.Satisfactory, Grade.Satisfactory, Grade.Good],
-};
+// const takeExam = (grade: number | undefined): boolean => {
+//   const temp: number = 0;
+//   if (typeof grade === typeof temp) {
+//     this.grades.push(grade);
+//     return true;
+//   }
+//   return false;
+// };
 
-const student3: Student = {
-  name: "Cäsar Schmidt",
-  age: 17,
-  courses: ["Biology", "Science", "Chemie"],
-  isEnrolled: true,
-  grades: [Grade.Satisfactory, Grade.Satisfactory, Grade.Good],
-};
+const student1 = createStudent(
+  "Anton Meier",
+  16,
+  ["Math", "Science", "History"],
+  true,
+  [Grade.Sufficient, undefined, Grade.Satisfactory, Grade.Satisfactory]
+);
+
+const student2 = createStudent(
+  "Berta Müller",
+  15,
+  ["Math", "Music", "History"],
+  true,
+  [Grade.Satisfactory, Grade.Satisfactory, Grade.Good]
+);
+
+const student3 = createStudent(
+  "Cäsar Schmidt",
+  17,
+  ["Biology", "Science", "Chemie"],
+  true,
+  [Grade.Satisfactory, Grade.Satisfactory, Grade.Good]
+);
 
 const students: Student[] = [student1, student2, student3];
 
@@ -47,6 +80,19 @@ const displayStudentInfo = (student: Student): void => {
   console.log(nameInfo);
   console.log("=".repeat(nameInfo.length));
   console.log("Courses: " + student.courses.join(", "));
+  console.log(
+    "Grades: " +
+      student.grades
+        .map((note) => {
+          if (note === undefined) {
+            return "*";
+          } else {
+            return `Grade.${Grade[note]} (${note})`;
+          }
+        })
+        .join(", ")
+  );
+  console.log("Exam valid: " + student.takeExam(4));
   console.log(
     "Grades: " +
       student.grades
