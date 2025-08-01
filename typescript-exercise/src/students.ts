cbc32623d47b0346eaa218c70fec5a6e90ff654f
ad-altun <adaltun@web.de>
2025-08-01 10:54:21 +0200

    typescript-exercise: update grade so that it can also get undefined as value. show it on console as '*'

--- typescript-exercise/src/students.ts
+++ typescript-exercise/src/students.ts
@@ -12,7 +12,7 @@ type Student = {
   age: number; // Age of the student
   courses: string[]; // List of courses the student is enrolled in
   isEnrolled: boolean; // Enrollment status of the student
-  grades: number[]; // Array of grades the student has received
+  grades: (number | undefined)[]; // Array of grades the student has received
 };
 
 const student1: Student = {
@@ -20,7 +20,7 @@ const student1: Student = {
   age: 20,
   courses: ["Math", "Science", "History"],
   isEnrolled: true,
-  grades: [Grade.Sufficient, Grade.Satisfactory, Grade.Satisfactory],
+  grades: [Grade.Sufficient, undefined, Grade.Satisfactory, Grade.Satisfactory],
 };
 
 const student2: Student = {
@@ -37,7 +37,16 @@ const displayStudentInfo = (student: Student): void => {
   console.group("student info: ");
   console.log(student.name + "( " + student.age + " )");
   console.log("================================ ");
-  console.log("Grades: " + student.grades.join(", "));
+  console.log(
+    "Grades: " +
+      student.grades
+        .map((note) => {
+          if (note === undefined) {
+            return "*";
+          } else return note;
+        })
+        .join(", ")
+  );
   console.groupEnd();
 };
 
