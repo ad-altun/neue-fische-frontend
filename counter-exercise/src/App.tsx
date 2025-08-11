8f426de6c8a7b4284b7d4155d4c02d3f0661c744
ad-altun <adaltun@web.de>
2025-08-11 14:50:06 +0200

    counter-exercise: add increment button and function

--- counter-exercise/src/App.tsx
+++ counter-exercise/src/App.tsx
@@ -1,6 +1,32 @@
+import { useState } from "react"
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = (): void => {
+    try {
+      setCounter(() => counter + 1);
+    } catch (error) {
+      console.log(error);
+      throw new Error("Function not implemented.");
+    }
+  }
+
+
+
   return (
-    <div>App</div>
+    <>
+      <div className="main">
+        <div className="value">
+          {counter}
+        </div>
+        <div className="buttons">
+          <button className="increase"
+            onClick={handleIncrement}
+          >Increment</button>
+
+        </div>
+      </div>
+    </>
   )
 }
