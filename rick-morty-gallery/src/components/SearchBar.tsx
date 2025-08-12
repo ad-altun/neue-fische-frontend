1793f472540972d66cecc704d68ae8c07f34cbf9
ad-altun <adaltun@web.de>
2025-08-12 11:39:32 +0200

    rick-morty-gallery: add new component

--- rick-morty-gallery/src/components/SearchBar.tsx
+++ rick-morty-gallery/src/components/SearchBar.tsx
@@ -1,6 +1,21 @@
+import { FaSearch } from 'react-icons/fa';
+import type { ChangeEvent } from "react"
 
-export default function SearchBar() {
+type SearchBarProbs = {
+    onChangeProb: (term: string) => void
+}
+
+export default function SearchBar({ onChangeProb }: SearchBarProbs) {
+
+    const handleChange = (term: string) => onChangeProb(term);
+
     return (
-        <div>SearchBar</div>
+        <div>
+            <FaSearch />
+            <h3>Search for character here: </h3>
+            <input type="text"
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleChange(e.target.value)} />
+        </div>
     )
 }
