f8050eb10ce32c3fc812add42223b93679992578
ad-altun <adaltun@web.de>
2025-08-12 15:30:30 +0200

    rick-morty-gallery: add routes

--- rick-morty-gallery/src/main.tsx
+++ rick-morty-gallery/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/characters' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
