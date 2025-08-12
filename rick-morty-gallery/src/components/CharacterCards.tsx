571f84c652c3bad4d9bd2d3e30ae23511f22e822
ad-altun <adaltun@web.de>
2025-08-12 09:28:38 +0200

    rick-morty-gallery: fetch data, display fields

--- rick-morty-gallery/src/components/CharacterCards.tsx
+++ rick-morty-gallery/src/components/CharacterCards.tsx
@@ -16,11 +16,47 @@ setRMCharacters(response);
 */
 
 export default function CharacterCards() {
+    const [rmCharacters, setRMCharacters] = useState<Character[]>([]);
 
+    const url: string = "https://rickandmortyapi.com/api/character/";
+
+    useEffect(() => {
+        const getRes = async () => {
+            try {
+                const res: CharactersResponse = await getRMCharacters(url);
+
+                const response: Character[] = res.results.map((item) => ({
+                    ...item,
+                    id: item.id,
+                    name: item.name,
+                    image: item.image,
+                }));
+
+                // setRMCharacters(res.results);
+                setRMCharacters(response);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+
+        getRes();
+
+    }, []);
 
     return (
         <>
-
+            <main className="main">
+                {
+                    rmCharacters.map((char) => {
+                        return (
+                            <div key={char.id}>
+                                <p>{char.name}</p>
+                                <img src={char.image} alt="" />
+                            </div>)
+                    }
+                    )
+                }
+            </main>
         </>
     )
 }
