26e5b5ecd18b6fd9b3f8c1b0877adc98cd34f855
ad-altun <adaltun@web.de>
2025-08-12 09:29:18 +0200

    rick-morty-gallery: add types

---
+++ rick-morty-gallery/src/types/types.ts
@@ -0,0 +1,40 @@
+export interface Info {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+}
+
+export interface LocationReference {
+  name: string;
+  url: string;
+}
+
+export type Status = "Alive" | "Dead" | "unknown";
+export type Gender = "Female" | "Male" | "Genderless" | "unknown";
+
+export interface Character {
+  id: number;
+  name: string;
+  status: Status;
+  species: string;
+  type: string;
+  gender: Gender;
+  origin: LocationReference;
+  location: LocationReference;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string; // ISO date string
+}
+
+export interface CharactersResponse {
+  info: Info;
+  results: Character[];
+}
+
+export interface CharacterPreview {
+  id: number;
+  name: string;
+  image: string;
+}
